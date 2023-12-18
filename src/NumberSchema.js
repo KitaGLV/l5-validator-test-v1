913581c99bed65702bd64614516ac542fdf926d5
Nikita Gulyaev <mikita.glv@gmail.com>
2023-12-18 11:26:55 +0300

    Done

--- src/NumberSchema.js
+++ src/NumberSchema.js
@@ -1,15 +1,16 @@
 export default class NumberSchema {
   validators = [(value) => typeof value === 'number'];
+
   isValid(value) {
     return this.validators.every((validator) => validator(value));
   }
 
-  even(value) {
+  even() {
     this.validators.push((value) => value % 2 === 0);
     return this;
   }
 
-  odd(value) {
+  odd() {
     this.validators.push((value) => value % 2 !== 0);
     return this;
   }
